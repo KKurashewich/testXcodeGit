89b709f5f33784aca3e3a3231d50060591210d63
Kevin J Kurashewich <kkurashewich@dlm-fl2imac5.rd.unr.edu>
2020-02-07 13:16:35 -0800

    Initial Commit

---
+++ gitTesting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gitTesting
+//
+//  Created by Kevin J Kurashewich on 2/7/20.
+//  Copyright © 2020 Kevin J Kurashewich. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
